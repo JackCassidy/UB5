dd2b65a1a99416c7aaad015079d7c64921aa07c8
JackCassidy <jackcassidy@cox.net>
2013-10-23 12:50:12 -0700

    Added a git ignore file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+in_tiny = File.new('../TinyData/tiny.tsv')
+puts 'Reading tiny.tsv'
+
+line = in_tiny.readline.chomp     # discard first line
+
+while !in_tiny.eof?
+  line = in_tiny.readline.chomp
+  sp = line.split("\t")
+  if sp.length < 4
+    next
+  else
+    raw_pep = sp[3]
+  end
+
+  @peptide = Peptide.new
+  puts raw_pep
+  ml = raw_pep.index('k')   # first modified Lysine
+  puts ml
+  if ml.nil?
+    next
+  else
+    @peptide.mod_loc = ml
+    @peptide.aseq = raw_pep.upcase
+    @peptide.save
+  end
+end   # while
