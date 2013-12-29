16ffaf2e3fa249e15a1342109ec44981397fbe75
Bill Cassidy <ordinaryacorn@gmail.com>
2013-12-28 17:11:46 -0800

    Added spec for model that seems to be failing.  Need to discuss with Jack.

--- spec/models/protein_spec.rb
+++ spec/models/protein_spec.rb
@@ -3,3 +3,13 @@ require 'spec_helper'
 describe Protein do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe "#parse_fasta_file" do
+  it "stores data from the input file to the database" do
+    expect(Protein.count).to eq(0)
+    path = Rails.root.join('spec', 'fixtures', 'tiny.fasta').to_s
+    fasta_file = File.new(path)
+    Protein.parse_fasta_file(fasta_file)
+    expect(Protein.count).to eq(10)
+  end
+end
