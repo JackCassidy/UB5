d2d45b8d451800ad847733e450d935764ab30a55
Bill Cassidy <ordinaryacorn@gmail.com>
2013-12-30 16:33:59 -0800

    re-enabled the load proteins feature spec we worked on for so long!

--- spec/feature/load_proteins_spec.rb
+++ spec/feature/load_proteins_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Load proteins from a file' do
 
-  xit 'should load proteins' do
+  it 'should load proteins' do
     visit proteins_select_file_path
 
     input_file_path = Rails.root.join('spec/fixtures/tiny.fasta')
