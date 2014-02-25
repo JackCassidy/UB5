8cda021095c6e58fa38ccc1ba6f93b293d58a013
JackCassidy <jackcassidy@cox.net>
2014-02-25 13:37:49 -0800

    figured out how to add a footer to all pages

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,9 @@ module UB5
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # from tip at stackoverflow
+    config.autoload_paths << "#{Rails.root}/lib"
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
