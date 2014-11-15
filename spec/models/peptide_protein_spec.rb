ad34a4b4561c887df3a2497a6f52a948b2f8f4f8
Bill Cassidy <pair+bill@pivotallabs.com>
2014-11-15 14:23:33 -0800

    added routine tests to peptide_protein and user models

--- spec/models/peptide_protein_spec.rb
+++ spec/models/peptide_protein_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe PeptideProtein, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to allow_mass_assignment_of :peptide_id }
+  it { is_expected.to allow_mass_assignment_of :protein_id }
+  it { is_expected.to allow_mass_assignment_of :protein_mod_site }
 
+  it { is_expected.to belong_to(:protein) }
+  it { is_expected.to belong_to(:peptide) }
+
+  it { is_expected.to validate_presence_of :peptide_id }
+  it { is_expected.to validate_presence_of :protein_id }
+  it { is_expected.to validate_presence_of :protein_mod_site }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to allow_mass_assignment_of :email}
+  it { is_expected.to allow_mass_assignment_of :password}
+  it { is_expected.to allow_mass_assignment_of :password_confirmation}
+  it { is_expected.to allow_mass_assignment_of :remember_me}
+
+  pending "add some devise tests"
 end
